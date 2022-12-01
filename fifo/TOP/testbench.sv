4eaf9f9a83a4f2e9cda84672d9ebf9f44231a4db
RaviSingh700 <89140263+RaviSingh700@users.noreply.github.com>
2022-12-01 15:09:44 +0530

    Update testbench.sv

--- fifo/TOP/testbench.sv
+++ fifo/TOP/testbench.sv
@@ -1,7 +1,7 @@
 `define number 190
 //`define number_1 5
 int error;
-//import uvm_pkg::*;
+import uvm_pkg::*;
 `include "uvm_macros.svh"
 `include "fifo_if.sv"
 `include "fifo_trans.sv"
@@ -19,14 +19,14 @@ int error;
 module uvm_top;
   bit clk;
   always #5 clk=~clk;
-  fifo_if i(clk);
+  fifo_if inter(clk);
   //flow fsm(.in(i.in),.reset(i.reset),.out(i.out),.sysclk(sysclk));
-  fifo fifi(.data_in(i.data_in),.clk(clk),.rst_n(i.rst_n),.push(i.push),.pop(i.pop),.push_err_on_full(i.push_err_on_full),.pop_err_on_empty(i.pop_err_on_empty),.full(i.full),.empty(i.empty),.data_out(i.data_out));
+  fifo fifi(.data_in(inter.data_in),.clk(clk),.rst_n(inter.rst_n),.push(inter.push),.pop(inter.pop),.push_err_on_full(inter.push_err_on_full),.pop_err_on_empty(inter.pop_err_on_empty),.full(inter.full),.empty(inter.empty),.data_out(inter.data_out));
   
   
   initial
     begin
-      uvm_config_db#(virtual fifo_if)::set(null,"","iface",i);
+      uvm_config_db#(virtual fifo_if)::set(null,"*","iface",inter);
     end
   initial
     begin
